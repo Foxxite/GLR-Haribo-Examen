184e25141715706997d9b16d43d05b28c0cf4da4
Foxxite <Dutch4Minecrafters@gmail.com>
2018-07-01 21:09:27 +0200

    More Stuff

---
+++ Assets/Hallu3.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hallu3 : MonoBehaviour {
+
+    public GameObject HalluBol;
+    public GameObject Candy1;
+    public GameObject Candy2;
+    public GameObject Candy3;
+    public GameObject Level;
+    public GameObject Walls;
+
+    // Use this for initialization
+    public void StartHallu()
+    {
+        StartCoroutine(StopHallu());
+    }
+
+    IEnumerator StopHallu()
+    {
+        Candy1.SetActive(false);
+        Candy1.SetActive(false);
+        Candy1.SetActive(false);
+        Level.SetActive(false);
+        Walls.SetActive(false);
+        HalluBol.SetActive(true);
+        yield return new WaitForSeconds(30);
+        Candy1.SetActive(true);
+        Candy1.SetActive(true);
+        Candy1.SetActive(true);
+        Level.SetActive(true);
+        Walls.SetActive(true);
+        HalluBol.SetActive(false);
+    }
+}
