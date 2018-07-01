8a26a7b24c67e331fa706ac3324cda6a2a69fdf8
Foxxite <Dutch4Minecrafters@gmail.com>
2018-07-01 19:44:24 +0200

    Stuff

---
+++ Assets/Scripts/EatCandy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EatCandy : MonoBehaviour {
+
+    public UnityEvent Hallu1;
+    public UnityEvent Hallu2;
+    public UnityEvent Hallu3;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Candy")
+        {
+            int r = Random.Range(0, 2);
+            if(r == 0)
+            {
+                Hallu1.Invoke();
+            }
+            else if (r == 1)
+            {
+                Hallu2.Invoke();
+            }
+            else
+            {
+                Hallu3.Invoke();
+            }
+        }
+    }
+}
